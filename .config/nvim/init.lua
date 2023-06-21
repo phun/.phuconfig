117b777bcae4a353f62d6b7adac22bf2658afc15
Phu Nguyen <phu.nguyen.mail@gmail.com>
2023-06-21 18:26:35 -0400

    2 to 4 spaces for indentations in init.lua (test commit key #2)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,8 +12,8 @@ local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 -- bootstrap lazy.nvim!
 if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+    require("core.bootstrap").gen_chadrc_template()
+    require("core.bootstrap").lazy(lazypath)
 end
 
 dofile(vim.g.base46_cache .. "defaults")
